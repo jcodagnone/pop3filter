b18213adcc2605f061d5705a04b0aec3abcfb366
juam <juam>
2001-11-01 01:56:39 +0000

    added nice colors to the levels :-)

--- lib/debug.c
+++ lib/debug.c
@@ -1,5 +1,5 @@
 /* 
- * $Id: debug.c,v 0.0 2001/06/13 14:54:59 juam Exp $
+ * $Id: debug.c,v 0.1 2001/11/01 01:56:39 juam Exp $
  *
  * Funciones para debugear el programa
  *
@@ -13,14 +13,38 @@
 #define DEBUG_LEVEL 20
 #endif
 
+/* configuration ala K&P */
+static int with_ansi_colors = 1 ;
+static const char *ansi_colors[]=
+{	"\x1b[31m",
+	"\x1b[32m",
+	"\x1b[33m",
+	"\x1b[34m",
+	"\x1b[35m",
+	"\x1b[36m",
+	"\x1b[37m",
+};
+
+#define NELEMS(x) (sizeof(x)/sizeof(*x))
+
+static const char *ansi_normal= "\x1b[39m\x1b[49m";
+
+
 void
 debug(unsigned level,const char *fmt,...)
-{  va_list ap;
+{	va_list ap;
 
-   if( level <= DEBUG_LEVEL )
-   {   va_start(ap,fmt);
-       vfprintf(stderr,fmt,ap);
-       va_end(ap);  
-   }
+	if( level <= DEBUG_LEVEL )
+	{	va_start(ap,fmt);
+		if( with_ansi_colors )
+		 	 fprintf(stderr,"%s",
+				ansi_colors[level%NELEMS(ansi_colors)]);
+		fprintf(stderr,"%2.2d:",level);
+		vfprintf(stderr,fmt,ap);
+		if( with_ansi_colors )
+        		fprintf(stderr,"%s",ansi_normal);
+
+   		va_end(ap);  
+	}
 
 }
