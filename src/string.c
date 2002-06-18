3fa4c3e594620ba8e243848d2877332eea326da9
juam <juam>
2002-06-18 15:58:55 +0000

    Added GPL header

--- src/string.c
+++ src/string.c
@@ -1,10 +1,29 @@
-
+/*
+ * string.c  -- String functions 
+ * $Id: string.c,v 1.2 2002/06/18 15:58:55 juam Exp $
+ *
+ * Copyright (C) 2002 by Juan F. Codagnone <juam@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 #include <stdlib.h>
 #include <string.h>
 
 #include "string.h"
 
-#define INIT_CHUNK	80
+#define INIT_CHUNK	512
 #define CHUNK_FACTOR	2
 
 struct stringCDT
@@ -141,4 +160,4 @@ main(void)
 
 }
 
-#endif
+#endif	/* _STRING_TESTDRIVER_ */
