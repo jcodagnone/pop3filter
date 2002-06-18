47cceaf6738f8f0e15fb1f600dbf9bda52877e21
juam <juam>
2002-06-18 17:07:32 +0000

    Added GPL header

--- lib/newopt.c
+++ lib/newopt.c
@@ -1,9 +1,23 @@
 /*
- * $Id: newopt.c,v 0.2 2001/10/03 02:12:22 juam Exp $
+ * newopt.c -- Command line options parser inspired in getopt(3)(in behavior)
  *
- * Command line options parser inspired in getopt(3) but unlike that,
- * this one _is_ THREAD SAFE
+ * $Id: newopt.c,v 0.3 2002/06/18 17:07:32 juam Exp $
  *
+ * Copyright (C) 2001 by Juan F. Codagnone <juam@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 #include <stdio.h>
 #include <stdlib.h>
