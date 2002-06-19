b292e27d95db06d67a06a09e6144e8d12a89fc2c
juam <juam>
2002-06-19 17:40:01 +0000

    *** empty log message ***

--- src/main.h
+++ src/main.h
@@ -1,11 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include "config.h"
 
 struct opt
 {	short lport;		/* listening port */
 	short rport;		/* remote port    */
 	const char * server;	/* remote machine */
-	const char *exec;	/* filter */
+	char *exec;		/* filter */
 	int fork;		/* go to background ? */
 };
 
