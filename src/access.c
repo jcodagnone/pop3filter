1d6f37f3a47df29263048d7e4b3187cf7d3a383b
juam <juam>
2002-06-23 21:12:50 +0000

    Host access control

---
+++ src/access.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "main.h"
+#include "trace.h"
+
+int
+client_access( int sock, const  struct opt *opt)
+{
+
+	return 1;
+}
+
---
+++ src/access.h
@@ -0,0 +1,8 @@
+#ifndef _ACCESS_H_
+#define _ACCESS_H_
+
+int
+client_access(int socket, const  struct opt *opt);
+
+#endif
+
